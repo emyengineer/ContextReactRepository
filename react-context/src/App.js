c531db6e62b4d5c2579d3c5f6f1569b2667efc7e
emyengineer <emyengineer@gmail.com>
2020-12-04 19:48:42 +0200

    commit before pull

--- react-context/src/App.js
+++ react-context/src/App.js
@@ -1,26 +1,33 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { ThemeContext, themes } from "./theme-context";
+import ThemedButton from "./themed-button";
+import React from "react";
+import {  Toolbar } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello My Lovely React</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: themes.light,
+    };
+
+    this.toggleTheme = () => {
+      this.setState((state) => ({
+        theme: state.theme === themes.dark ? themes.light : themes.dark,
+      }));
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <Toolbar changeTheme={this.toggleTheme} />
+        </ThemeContext.Provider>
+        <div>
+          <ThemedButton />
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
